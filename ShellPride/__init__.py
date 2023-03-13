259753f7519859a9957e3ceed6a37ca0c0f58ff2
ccmorenos <ccatamorenos@gmail.com>
2023-03-13 12:17:18 -0500

    Add function to list the available flags

--- ShellPride/__init__.py
+++ ShellPride/__init__.py
@@ -8,6 +8,21 @@ from ShellPride.messages import flags_messages
 from ShellPride.colors import ColorsBase
 
 
+def print_flag_list():
+    """Print the list of available pride flags."""
+    print("Possible flag options:\n\n", end="\t")
+
+    i = 1
+    for flag_name in flags_messages.keys():
+        print(f"* {flag_name}".ljust(20), end=" ")
+        if i % 5 == 0:
+            print(end="\n\t")
+
+        i += 1
+
+    print()
+
+
 def check_flag(name):
     """Check that the flag exists."""
     if name in flags_messages.keys():
@@ -15,93 +30,24 @@ def check_flag(name):
 
     else:
         print(f"Error: Flag {name} not recognized.\n", file=sys.stderr)
-
-        print("Possible flag options:\n\n", end="\t")
-
-        i = 1
-        for flag_name in flags_messages.keys():
-            print(flag_name, end=" ")
-            if i % 5 == 0:
-                print(end="\n\t")
-
-            i += 1
-
-        print()
+        print_flag_list()
         exit(1)
 
 
-# Create the parser for the console arguments.
-parser = argparse.ArgumentParser(
-    prog="shell_pride",
-    description="Print shell decorator.",
-    formatter_class=argparse.RawTextHelpFormatter
-)
+def add_decorations(args):
+    """
+    Add decorations for the print.
 
-# Add argument for the flags that will be printed.
-parser.add_argument(
-    "-f",  "--flags", type=check_flag, nargs="+", required=True,
-    help="List of flags to be shown"
-)
+    Parameters
+    ----------
+    arg: Shell arguments.
+        Arguments passed in the shell.
 
-# Add argument for the flags that will be printed.
-parser.add_argument(
-    "-L",  "--length", type=int, default=25,
-    help="Length of the flags"
-)
-
-# Add argument for the decoration color.
-parser.add_argument(
-    "-d",  "--decorator", type=str, default="#F5A9B8",
-    help="Add the color for the decorator letters"
-)
-
-# Add argument for the name.
-parser.add_argument("-n",  "--name", type=str, help="Add your name")
-
-# Add argument for the pronouns.
-parser.add_argument("-p",  "--pronouns", type=str, help="Add your pronouns")
-
-# Add argument for the zodiac sign.
-parser.add_argument(
-    "-zs",  "--zodiac_solar", type=str, help="Add your solar zodiac sign"
-)
-parser.add_argument(
-    "-za",  "--zodiac_ascendant", type=str,
-    help="Add your ascendant zodiac sign"
-)
-parser.add_argument(
-    "-zm",  "--zodiac_moon", type=str, help="Add your moon zodiac sign"
-)
-
-# Parse arguments.
-args = parser.parse_args()
-
-
-def main():
-    """Print pride flags."""
+    """
     # Create a base color.
     base_colors = ColorsBase({"decorations": args.decorator})
 
-    # Initialize the flags.
-    added_flags = [
-        flags_names[
-            re.search(r"([^\W_\d]+)(_var[\d+])?", name)[1]
-        ](length=args.length) for name in args.flags
-    ]
-
-    # Get max length.
-    max_stripes = max([
-        flag.stripes
-        for flag in added_flags
-    ])
-
-    # Get Stripes.
-    stripes = [
-        flag.center(flags_messages[name])
-        for flag, name in zip(added_flags, args.flags)
-    ]
-
-    # Add decorations.
+    # Create an empty list for the decorations.
     decorations = []
 
     if args.name:
@@ -138,6 +84,88 @@ def main():
             f"{base_colors.reset + base_colors.get_frg('decorations')}"
             f"{args.zodiac_moon}"
         )
+
+    return decorations
+
+
+# Create the parser for the console arguments.
+parser = argparse.ArgumentParser(
+    prog="shell_pride",
+    description="Print shell decorator.",
+    formatter_class=argparse.RawTextHelpFormatter
+)
+
+# Add argument for the flags that will be printed.
+parser.add_argument(
+    "-f",  "--flags", type=check_flag, nargs="+", required=True,
+    help="List of flags to be shown."
+)
+
+# Add argument for the flags that will be printed.
+parser.add_argument(
+    "-L",  "--length", type=int, default=25,
+    help="Length of the flags."
+)
+
+# Add argument for the flags that will be printed.
+parser.add_argument(
+    "-lf",  "--list_flags", action="store_true",
+    help="List all the posible flags and exit."
+)
+
+# Add argument for the decoration color.
+parser.add_argument(
+    "-d",  "--decorator", type=str, default="#F5A9B8",
+    help="Add the color for the decorator letters."
+)
+
+# Add argument for the name.
+parser.add_argument("-n",  "--name", type=str, help="Add your name.")
+
+# Add argument for the pronouns.
+parser.add_argument("-p",  "--pronouns", type=str, help="Add your pronouns.")
+
+# Add argument for the zodiac sign.
+parser.add_argument(
+    "-zs",  "--zodiac_solar", type=str, help="Add your solar zodiac sign."
+)
+parser.add_argument(
+    "-za",  "--zodiac_ascendant", type=str,
+    help="Add your ascendant zodiac sign."
+)
+parser.add_argument(
+    "-zm",  "--zodiac_moon", type=str, help="Add your moon zodiac sign."
+)
+
+# Parse arguments.
+args = parser.parse_args()
+
+
+def main():
+    """Print pride flags."""
+    # List pride flags.
+    if args.list_flags:
+        print_flag_list()
+        exit(0)
+
+    # Initialize the flags.
+    added_flags = [
+        flags_names[
+            re.search(r"([^\W_\d]+)(_var[\d+])?", name)[1]
+        ](length=args.length) for name in args.flags
+    ]
+
+    # Get max length.
+    max_stripes = max([flag.stripes for flag in added_flags])
+
+    # Get Stripes.
+    stripes = [
+        flag.center(flags_messages[name])
+        for flag, name in zip(added_flags, args.flags)
+    ]
+
+    # Add decorations.
+    decorations = add_decorations(args)
 
     # Print the flags.
     print()
